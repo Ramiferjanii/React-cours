a4415eb63e1bbeb7da84186eadfaf87bf894adbe
Rami Ferjani <ramiferjani.20@gmail.com>
2025-02-06 22:10:13 +0000

    Add files via upload

---
+++ src/components/JSXRules.jsx
@@ -0,0 +1,13 @@
+
+const JSXRules = () => {
+  return (
+    <div>
+      <h1>JSX rules </h1>
+      <p>JSX must return a simple parent element </p>
+      <p>JSX element must return proprely closed </p>
+      <p>JSX attributes must be properly closed camelCase (e.g className instead of class</p>
+    </div>
+  )
+}
+
+export default JSXRules
