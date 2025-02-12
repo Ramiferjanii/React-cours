7c60b3124341045ce1b16a21f8b62d82a43f5b31
Rami Ben Ferjani <ramiferjani.20@gmail.com>
2025-02-12 21:10:09 +0000

    Add files via upload

---
+++ UseState/src/App.jsx
@@ -0,0 +1,20 @@
+import Counter from "./components/Counter";
+import Profile from "./components/Profile";
+import ToDoList from "./components/ToDoList";
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Counter/> 
+      <ToDoList/>
+      <Profile/>
+    </div>
+  )
+}
+
+export default App ; 
