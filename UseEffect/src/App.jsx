4527ee08829faeac82742d8488bae04692d2f00b
Rami Ben Ferjani <ramiferjani.20@gmail.com>
2025-02-14 20:06:17 +0000

    Add files via upload

---
+++ UseEffect/src/App.jsx
@@ -0,0 +1,19 @@
+import BasicEffect from "./components/BasicEffect";
+import CounterEffect from "./components/CounterEffect";
+
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <BasicEffect/>
+      <CounterEffect/>
+      <BasicEffect/> 
+    </div>
+  )
+}
+
+export default App ; 
