d08ec0138d3794f3911586247d01943d1ec98cc7
Rami Ben Ferjani <ramiferjani.20@gmail.com>
2025-03-22 12:14:47 +0000

    Add files via upload

--- 03. React & TypeScript/6. useRef, Forms, Events/Live Coding/components/Form.tsx
+++ 03. React & TypeScript/6. useRef, Forms, Events/Live Coding/components/Form.tsx
@@ -1,56 +1,55 @@
-"use client";
 
-import { useRef, type FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react"
+
 
 type formData = {
-  name: string;
-  email: string;
-  password: string;
-};
+    name: string , 
+    email : string , 
+    password: string 
+}
+
+
+
 
 const Form = () => {
-  const [submittedData, setSubmittedData] = useState<formData>({
-    name: "",
-    email: "",
-    password: "",
-  });
+        const [submitted, setSubmitted] = useState<formData>(
+            {
+                name: '' ,
+                email : '' ,
+                password : '' 
+            }
+        )
 
-  const name = useRef<HTMLInputElement>(null);
-  const email = useRef<HTMLInputElement>(null);
-  const password = useRef<HTMLInputElement>(null);
+const name = useRef<HTMLInputElement>(null)
+const email = useRef<HTMLInputElement> (null)
+const password = useRef<HTMLInputElement>(null)
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const nameVal = name.current!.value;
-    const emailVal = email.current!.value;
-    const passwordVal = password.current!.value;
-
-    setSubmittedData({
-      name: nameVal,
-      email: emailVal,
-      password: passwordVal,
-    });
-
-    console.log(nameVal);
-    console.log(emailVal);
-    console.log(passwordVal);
-  };
-
+const handleSubmit = (event : FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const nameValue = name.current!.value
+    const emailValue = email.current!.value
+    const passwordValue = password.current!.value
+    setSubmitted({
+        name: nameValue,
+        email: emailValue,
+        password: passwordValue
+    })
+}
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Enter your name" ref={name} />
-      <input type="email" placeholder="Enter your email" ref={email} />
-      <input type="password" placeholder="Enter your password" ref={password} />
-      <button type="submit">Submit</button>
+    <form className="form-container" onSubmit={handleSubmit}>
+    <input className="form-input" type="text" placeholder="Enter you name "  ref={name} />
+    <input className="form-input" type="email" placeholder="Enter you email "  ref={email} />
+    <input className="form-input" type="password" placeholder="Enter you password "  ref={password} />
+    <button type="submit" className="submit-btn" >Submit</button>
 
-      <section>
-        <h1>Name: {submittedData.name}</h1>
-        <h1>Email: {submittedData.email}</h1>
-        <h1>Password: {submittedData.password}</h1>
-      </section>
+
+    <section>
+        <h1>Name : {submitted.name } </h1>
+        <h1>email : {submitted.email }</h1>
+        <h1>Password : {submitted.password }</h1>
+    </section>
     </form>
-  );
-};
+  )
+}
 
-export default Form;
+export default Form
